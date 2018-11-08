e72d3bf73edc87d2d1fba0354ec4db05e0739708
Eugene Zadyra <untrance@gmail.com>
2018-11-08 18:23:17 +0100

    Added shell script to simplify building release binaries

---
+++ release.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export GOARCH=amd64
+export GOOS=windows
+go build -o rezerwacje-duw-go-win64.exe
+export GOOS=linux
+go build -o rezerwacje-duw-go-linux64
+export GOOS=darwin
+go build -o rezerwacje-duw-go-osx
+
+export GOARCH=386
+export GOOS=windows
+go build -o rezerwacje-duw-go-win32.exe
+export GOOS=linux
+go build -o rezerwacje-duw-go-linux32
