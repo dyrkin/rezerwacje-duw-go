0b4d349a61404d97691186cc012b36bb36aedf06
Eugene Zadyra <untrance@gmail.com>
2018-12-19 23:26:00 +0100

    Fixed paths in release.sh

--- release.sh
+++ release.sh
@@ -2,15 +2,15 @@
 
 export GOARCH=amd64
 export GOOS=windows
-go build -o rezerwacje-duw-go-win64.exe
+go build -o bin/rezerwacje-duw-go-win64.exe
 export GOOS=linux
-go build -o rezerwacje-duw-go-linux64
+go build -o bin/rezerwacje-duw-go-linux64
 export GOOS=darwin
-go build -o rezerwacje-duw-go-osx
+go build -o bin/rezerwacje-duw-go-osx
 export GOARCH=386
 export GOOS=windows
-go build -o rezerwacje-duw-go-win32.exe
+go build -o bin/rezerwacje-duw-go-win32.exe
 export GOOS=linux
-go build -o rezerwacje-duw-go-linux32
-chmod +x rezerwacje-duw-go-linux*
-chmod +x rezerwacje-duw-go-osx
+go build -o bin/rezerwacje-duw-go-linux32
+chmod +x bin/rezerwacje-duw-go-linux*
+chmod +x bin/rezerwacje-duw-go-osx
